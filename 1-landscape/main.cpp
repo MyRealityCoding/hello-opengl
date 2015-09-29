c0a16589d049460a894cf257c88465dd2c1dfd82
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-09-29 11:36:55 +0200

    Initialize window

--- 1-landscape/main.cpp
+++ 1-landscape/main.cpp
@@ -1,9 +1,31 @@
 #include <GL/glut.h>
 
+void display()
+{
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glFlush();
+  glutSwapBuffers();
+}
+
+void reshape(GLsizei w, GLsizei h)
+{
+
+}
+
 int main(int argc, char** argv)
 {
   glutInit(&argc, argv);
+
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+  glutInitWindowSize(800, 600);
+  glutInitWindowPosition(100, 100);
+
   glutCreateWindow("OpenGL - Landscape");
+
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
   glutMainLoop();
   return 0;
 }
